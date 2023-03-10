3a4ccd585cfba76a88a9e92b67e95dc2dd2e49e9
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2023-03-10 07:26:46 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/_default/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      nav: ["Izy", "sans-serif"],
+      heading: ["izyan_bodu", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
